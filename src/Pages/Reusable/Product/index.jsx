629f724f3bf607bf5ad02b6589eb7bb24108dea3
autonomous <autonomoustechnepal@gmail.com>
2023-11-01 17:42:25 +0545

    work on progress

--- src/Pages/Reusable/Product/index.jsx
+++ src/Pages/Reusable/Product/index.jsx
@@ -1,18 +1,91 @@
-import { useState , useEffect, useCallback}  from 'react';
+import { useState, useEffect, useCallback } from "react";
 import { Card } from "antd";
-// import { UseEmblaCarouselType } from 'embla-carousel-react';
+import useEmblaCarousel from "embla-carousel-react";
 
 import AppLayout from "../../../Layouts/AppLayout";
 
 const ProductPage = () => {
+  const options = {};
+  const [selectedIndex, setSelectedIndex] = useState(0);
+  const [emblaMainRef, emblaMainApi] = useEmblaCarousel(options);
+  const [emblaThumbsRef, emblaThumbsApi] = useEmblaCarousel({
+    containScroll: "keepSnaps",
+    dragFree: true,
+  });
+
+  // scroll with thumb
+  const onThumbClick = useCallback(
+    (index) => {
+      if (!emblaMainApi || !emblaThumbsApi) {
+        emblaMainApi.scrollTo(index);
+      }
+    },
+    [emblaMainApi, emblaThumbsApi]
+  );
+
+  const onSelect = useCallback(() => {
+    if (!emblaMainApi || !emblaThumbsApi) return;
+
+    setSelectedIndex(emblaMainApi.selectedScrollSnap());
+    emblaThumbsApi.scrollTo(emblaMainApi.selectedScrollSnap());
+  }, [emblaMainApi, emblaThumbsApi, setSelectedIndex]);
+
+  useEffect(() => {
+    if (!emblaMainApi) return;
+
+    onSelect();
+    emblaMainApi.on("select", onSelect);
+    emblaMainApi.on("reInit", onSelect);
+  }, [emblaMainApi, onSelect]);
+
   return (
     <>
       <AppLayout>
         <section className="container mx-auto mt-5">
           <Card>
             <div className="flex">
-              <div>1</div>
-              <div>2</div>
+              <div>
+                <div className="embla">
+                  <div className="embla__viewport" ref={emblaMainRef}>
+                    <div className="embla__container">
+                      {slides.map((index) => (
+                        <div className="embla__slide" key={index}>
+                          <div className="embla__slide__number">
+                            <span>{index + 1}</span>
+                          </div>
+                          <img
+                            className="embla__slide__img"
+                            src={imageByIndex(index)}
+                            alt="Your alt text"
+                          />
+                        </div>
+                      ))}
+                    </div>
+                  </div>
+
+                  {/* <div className="embla-thumbs">
+                    <div
+                      className="embla-thumbs__viewport"
+                      ref={emblaThumbsRef}
+                    >
+                      <div className="embla-thumbs__container">
+                        {slides.map((index) => (
+                          <Thumb
+                            onClick={() => onThumbClick(index)}
+                            selected={index === selectedIndex}
+                            index={index}
+                            imgSrc={imageByIndex(index)}
+                            key={index}
+                          />
+                        ))}
+                      </div>
+                    </div>
+                  </div> */}
+                </div>
+              </div>
+              <div>
+                <h1>Product Name</h1>
+              </div>
             </div>
           </Card>
         </section>
